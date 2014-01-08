0060d77efe0371458ddfd2a71f58b7e54f3f9f42
Denis Kutukov <mail@dskut.ru>
2014-01-08 14:13:56 +0400

    make library header-only

--- include/pool.h
+++ include/pool.h
@@ -7,13 +7,59 @@
 
 #include "task_queue.h"
 
+class Worker {
+public:
+    Worker(TaskQueue* const tasks, boost::atomic<bool>* const isPoolStopped)
+        : tasks_(tasks)
+        , isPoolStopped_(isPoolStopped)
+    {}
+
+    void operator()() {
+        while (!	*isPoolStopped_) {
+            popTaskAndRun();
+        }
+        popTaskAndRun();
+    }
+
+private:
+    void popTaskAndRun() {
+        IRunnable* task;
+        while (tasks_->pop(task)) {
+            task->run();
+            delete task;
+        }
+    }
+
+    TaskQueue* const tasks_;
+    boost::atomic<bool>* const isPoolStopped_;
+};
+
 class ThreadPool {
 public:
-    ~ThreadPool();
+    ~ThreadPool() {
+        stop();
+    }
 
-    void start(size_t threadsCount);
-    void stop();
-    void add(IRunnable* task);
+    void start(size_t threadsCount) {
+        isStopped_ = false;
+        threads_.clear();
+        for (size_t i = 0; i < threadsCount; ++i) {
+            threads_.push_back(std::thread(Worker(&tasks_, &isStopped_)));
+        }
+    }
+
+    void stop() {
+        isStopped_ = true;
+        for (std::thread& thread: threads_) {
+            thread.join();
+        }
+        threads_.clear();
+    }
+
+    void add(IRunnable* task) {
+        while (!tasks_.push(task)) {
+        }
+    }
 
 private:
     std::vector<std::thread> threads_;
